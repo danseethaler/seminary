0db30a84e50da68cb83b3becf3fc7f397d8872d2
Dan Seethaler <dan@seethalers.com>
2017-09-28 09:18:52 -0400

    ut package update and cleanup

--- src/pages/devotional.js
+++ src/pages/devotional.js
@@ -2,11 +2,7 @@ import React from 'react'
 import Link from 'gatsby-link'
 import { DateDiv } from '../components/bits'
 import { dates, assignments, roster } from '../data'
-
 import { mapOb } from '@danseethaler/ut'
-// import divvyUp from 'divvy-up';
-
-// const schedule = divvyUp(dates, assignments, roster);
 import schedule from '../data/schedule'
 
 const DevotionalPage = () => {
